74d0b357843acd16d0ed955537a7bc7ec95e9b8c
BimoSora <bimosora3@gmail.com>
2021-12-13 12:17:05 +0700

    first commit

--- index.js
+++ index.js
@@ -141,16 +141,21 @@ bot.start(async(ctx)=>{
                     }else if(res2.type=='photo'){
                         ctx.deleteMessage()
                         if(!res2.caption) {
-                            setTimeout(captionFunction2, 1000)
-                            return ctx.replyWithPhoto(res2.file_id,{caption: `#file${res2.uniqueId} #size${res2.file_size}`,
-                                parse_mode:'HTML',
-                                disable_web_page_preview: true,
-                                reply_markup:{
-                                    inline_keyboard:[
-                                        [{text: res2.type, url: `https://t.me/c/${ctx.chat.id}/${ctx.update_id}`}]
-                                    ]
+                            if(ctx.chat.type == 'group' || ctx.chat.type == 'supergroup') {
+                                const message_group = `https://t.me/c/${ctx.chat.id}/${ctx.update_id}`
+                                setTimeout(captionFunction2, 1000)
+                                if(ctx.chat.type == 'private') {
+                                    return ctx.replyWithPhoto(res2.file_id,{caption: `#file${res2.uniqueId} #size${res2.file_size}`,
+                                        parse_mode:'HTML',
+                                        disable_web_page_preview: true,
+                                        reply_markup:{
+                                            inline_keyboard:[
+                                                [{text: res2.type, url: `${message_group}`}]
+                                            ]
+                                        }
+                                    });
                                 }
-                            });
+                            }
                         }
                         ctx.replyWithPhoto(res2.file_id,{caption: `${res2.caption} \n\n#file${res2.uniqueId} #size${res2.file_size}`,
                             parse_mode:'HTML',
