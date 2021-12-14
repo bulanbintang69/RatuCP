ec4882828399e4d79dbc45cf9fc997d2faca0a9f
BimoSora <bimosora3@gmail.com>
2021-12-14 13:33:39 +0700

    first commit

---
+++ config.js
@@ -0,0 +1,34 @@
+module.exports = {
+        //Bot token provided by @botfather.
+    TOKEN: '',
+
+    //Enter the domain / ip that has been set in nginx.
+    DOMAIN: '',
+
+    //Enter the port that has been set in nginx.
+    PORT: '',
+
+    //Get the mongodb database url from https://www.mongodb.com/cloud/atlas with database name: mediaFile and collection name: fileBackup.
+    DB_URL: '',
+
+        //Enter your account id, if there is a 2nd and 3rd admin, fill in ADMIN1, ADMIN2, ADMIN3 and ADMIN4.
+    ADMIN: '',
+
+        //Enter your bot username without @ and must be lowercase.
+    BOTUSERNAME: '',
+
+        //Create private channel and enter channel id to monitor files and get user and file data.
+    LOG_CHANNEL: '',
+
+        //Caption when the media link sent is pressed.
+    CAPTIONLINK: '',
+
+        //Enter a message before the user joins.
+    WELCOMEJOINBOT: '',
+
+        //Enter a welcome message when a user joins.
+    MESSAGEWELCOMEBOT: '',
+
+        //Enter group channel ID to identify bots can read logged in users.
+    CHANNELJOIN: ''
+}
