7af26cb049eda2b337ebb9638b8dbb19ed3b462b
BimoSora <bimosora3@gmail.com>
2021-12-16 21:00:07 +0700

    first commit

--- index.js
+++ index.js
@@ -1871,9 +1871,10 @@ bot.on('photo', async(ctx, next) => {
     }
 
     if(ctx.from.id == process.env.ADMIN || ctx.from.id == process.env.ADMIN1 || ctx.from.id == process.env.ADMIN2 || ctx.from.id == process.env.ADMIN3 || ctx.from.id == process.env.ADMIN4){
-        if(ctx.chat.type == 'supergroup' && ctx.chat.id != channelId){
-            console.log('halo')
-        }else if(ctx.chat.type == 'supergroup' && ctx.chat.id == channelId){
+        if(ctx.chat.type == 'supergroup'){
+            if(ctx.chat.id == channelId){
+                console.log('halo')
+            }
             if(photo[1].file_name == undefined){
                 if(ctx.chat.type == 'private'){
                     await saver.checkFile(`${photo[1].file_unique_id}`).then(async res => {
