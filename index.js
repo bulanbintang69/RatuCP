5957d32572a5d3c3ba0939a9648c6dcd98e86e67
BimoSora <bimosora3@gmail.com>
2021-12-17 00:55:16 +0700

    first commit

--- index.js
+++ index.js
@@ -2100,15 +2100,25 @@ bot.on('photo', async(ctx, next) => {
                         }
                     }else{
                         if(ctx.chat.type == 'private') {
-                            if(!profile2 || profile2.total_count == 0)
-                                return await ctx.reply(`<a href="tg://user?id=${ctx.from.id}">${first_name(ctx)} ${last_name(ctx)}</a> \n\n${welcomejoin(ctx)}`,{
-                                    parse_mode:'HTML',
-                                    disable_web_page_preview: true
-                                })
-                                await ctx.replyWithPhoto(profile2.photos[0][0].file_id,{caption: `<a href="tg://user?id=${ctx.from.id}">${first_name(ctx)} ${last_name(ctx)}</a> \n\n${welcomejoin(ctx)}`,
-                                    parse_mode:'HTML',
-                                    disable_web_page_preview: true
-                                })
+                            try{
+                                var botStatus = await bot.telegram.getChatMember(channelId, ctx.botInfo.id)
+                                var member = await bot.telegram.getChatMember(channelId, ctx.from.id)
+                                //console.log(member);
+                                if(member.status == 'restricted' || member.status == 'left' || member.status == 'kicked'){
+                                    await ctx.reply(`${messagebotnoaddgroup(ctx)}`)
+                                }else{
+                                    if(!profile2 || profile2.total_count == 0)
+                                        return await ctx.reply(`<a href="tg://user?id=${ctx.from.id}">${first_name(ctx)} ${last_name(ctx)}</a> \n\n${welcomejoin(ctx)}`,{
+                                            parse_mode:'HTML',
+                                            disable_web_page_preview: true
+                                        })
+                                        await ctx.replyWithPhoto(profile2.photos[0][0].file_id,{caption: `<a href="tg://user?id=${ctx.from.id}">${first_name(ctx)} ${last_name(ctx)}</a> \n\n${welcomejoin(ctx)}`,
+                                            parse_mode:'HTML',
+                                            disable_web_page_preview: true
+                                        })
+                                }catch(error){
+                                    await ctx.reply(`${messagebotnoaddgroup(ctx)}`)
+                                }
                         }
                     }
                 })
