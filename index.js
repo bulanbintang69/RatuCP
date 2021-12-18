b83b2595f5788d156d1deacecb4a8fe060ea99b3
BimoSora <bimosora3@gmail.com>
2021-12-18 20:59:50 +0700

    Update index.js

--- index.js
+++ index.js
@@ -845,8 +845,7 @@ bot.command('send',async(ctx)=>{
                 //console.log(memberstatus);
 
                 if(ctx.chat.type == 'group' || ctx.chat.type == 'supergroup') {
-                    if(memberstatus.status == 'creator' || memberstatus.status == 'administrator'){
-                        await ctx.deleteMessage()
+                    if(memberstatus.status == 'creator' || memberstatus.status == 'administrator'){                        
                         if(ctx.message.reply_to_message == undefined){
                             const str = ctx.message.text;
                             const words = str.split(/ +/g);
