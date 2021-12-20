5af619e6112fa3d642898a1c23dcc86fda1e912a
BimoSora <bimosora3@gmail.com>
2021-12-20 07:50:54 +0700

    Update index.js

--- index.js
+++ index.js
@@ -392,18 +392,17 @@ bot.start(async(ctx)=>{
 
 //TEST BOT
 bot.hears(/ping/i,async(ctx)=>{
-    if(ctx.chat.type == 'private') {    
+    if(ctx.chat.type == 'private') {
         await saver.checkBan(`${ctx.from.id}`).then(async res => {
             //console.log(res);
             if(res == true) {
                 if(ctx.chat.type == 'private') {
-                    await ctx.deleteMessage()
                     await ctx.reply(`${messagebanned(ctx)}`)
                 }
             }else{
-                await ctx.deleteMessage()
                 let chatId = ctx.message.from.id;
                 let opts = {
+                    reply_to_message_id: ctx.message.message_id,
                     reply_markup:{
                         inline_keyboard: [[{text:'OK',callback_data:'PONG'}]]
                     }
@@ -903,23 +902,21 @@ bot.command('getid',async(ctx)=>{
         }, 1_000);
     });
   
-    if(ctx.chat.type == 'private') {       
+    if(ctx.chat.type == 'private') {
+        await ctx.deleteMessage()
         const profile4 = await bot.telegram.getUserProfilePhotos(ctx.from.id)
         await saver.checkBan(`${ctx.from.id}`).then(async res => {
             //console.log(res);
             if(res == true) {
                 if(ctx.chat.type == 'private') {
-                    await ctx.deleteMessage()
                     await ctx.reply(`${messagebanned(ctx)}`)
                 }
             }else{
                 if(!profile4 || profile4.total_count == 0){
-                    await ctx.deleteMessage()
                     await ctx.reply(`<b>Name:</b> <a href="tg://user?id=${ctx.from.id}">${first_name(ctx)} ${last_name(ctx)}</a>\n<b>Username:</b> ${username(ctx)}\n<b>ID:</b> ${ctx.from.id}`,{
                         parse_mode:'HTML'  
                     })
                 }else{
-                    await ctx.deleteMessage()
                     await ctx.replyWithPhoto(profile4.photos[0][0].file_id,{caption: `<b>Name:</b> <a href="tg://user?id=${ctx.from.id}">${first_name(ctx)} ${last_name(ctx)}</a>\n<b>Username:</b> ${username(ctx)}\n<b>ID:</b> ${ctx.from.id}`,
                         parse_mode:'HTML'
                     })
@@ -1042,7 +1039,7 @@ bot.command('broadcast',async(ctx)=>{
             //broadcasting
             totalBroadCast = 0
             totalFail = []
-
+            
             //creating function for broadcasting and to know bot user status
             async function broadcast(text) {
                 for (const users of userId) {
@@ -2342,12 +2339,10 @@ bot.command('stats',async(ctx)=>{
 })
  
 //heroku config
-//domain = `${process.env.DOMAIN}.herokuapp.com`
-//bot.launch({
-//    webhook:{
-//       domain:domain,
-//        port:Number(process.env.PORT) 
-//    }
-//})
-
-bot.launch()
+domain = `${process.env.DOMAIN}.herokuapp.com`
+bot.launch({
+    webhook:{
+       domain:domain,
+        port:Number(process.env.PORT) 
+    }
+})
