1aa886da20b08d2a13c0442ed61de8290c414ac6
BimoSora <bimosora3@gmail.com>
2021-12-22 17:03:05 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1508,7 +1508,7 @@ bot.on('video', async(ctx, next) => {
     await new Promise((resolve, reject) =>{
         setTimeout(()=>{
             return resolve("Result");
-        }, 2_000);
+        }, 5_000);
     });
     await next();
   
