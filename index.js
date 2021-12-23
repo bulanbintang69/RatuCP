979fa4a3c51c669c2ba6db76d9fe7e37af055954
BimoSora <bimosora3@gmail.com>
2021-12-23 20:56:03 +0700

    first commit

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ db.connect((err) => {
 })
 
 const limiter = rateLimit({
-    windowMs: 15 * 60 * 1000, // 15 minutes
+    windowMs: 2000, // 15 minutes
     max: 100 // limit each IP to 100 requests per windowMs
 });
   
