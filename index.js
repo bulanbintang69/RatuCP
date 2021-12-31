b50c6b32885723e28cf2a318dc8c7ea49a037f3d
BimoSora <bimosora3@gmail.com>
2021-12-31 16:18:45 +0700

    first commit

--- index.js
+++ index.js
@@ -1305,7 +1305,7 @@ bot.on('video', async(ctx) => {
             }
             
             const result2 = await saver.checkFile(`${video.file_unique_id}`);
-                let result = ${result2}.replace(/-/g, '_')
+                let result = `${result2}`.replace(/-/g, '_')
                 //console.log(res);
                 if(res == true) {
                     await ctx.reply(`File already exists. #file${result}`)
