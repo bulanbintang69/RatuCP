2a8c3d5f862bb7ca8f7f238d3bff9d9053391be2
BimoSora <bimosora3@gmail.com>
2022-01-04 11:23:58 +0700

    first commit

--- index.js
+++ index.js
@@ -1283,6 +1283,25 @@ bot.on('video', async(ctx) => {
     if(ctx.chat.type == 'private') {
         if(ctx.from.id == Number(process.env.ADMIN) || ctx.from.id == Number(process.env.ADMIN1) || ctx.from.id == Number(process.env.ADMIN2) || ctx.from.id == Number(process.env.ADMIN3) || ctx.from.id == Number(process.env.ADMIN4)){
             var video = ctx.message.video
+            if(video.file_name == undefined){
+                var file_name2 = `${today2(ctx)}`;
+                if(ctx.message.caption == undefined){
+                    var caption2 =  ``;
+                }else{
+                    var caption2 =  `\n\n${ctx.message.caption}`;
+                }
+            }else{
+                var exstension2 = video.file_name;
+                var regex2 = /\.[A-Za-z0-9]+$/gm
+                var vidtext2 = exstension2.replace(regex2, '');
+                
+                var file_name2 = `${vidtext2}`;
+                if(ctx.message.caption == undefined){
+                    var caption2 =  ``;
+                }else{
+                    var caption2 =  `\n\n${ctx.message.caption}`;
+                }
+            }
             
             await saver.checkFile(`${video.file_unique_id}`).then(async res => {
                 let result = `${video.file_unique_id}`.replace(/-/g, '_');
@@ -1290,27 +1309,6 @@ bot.on('video', async(ctx) => {
                 if(res == true) {
                     await ctx.reply(`File already exists. #file${result}`)
                 }else{
-
-                    if(video.file_name == undefined){
-                        var file_name2 = `${today2(ctx)}`;
-                        if(ctx.message.caption == undefined){
-                            var caption2 =  ``;
-                        }else{
-                            var caption2 =  `\n\n${ctx.message.caption}`;
-                        }
-                    }else{
-                        var exstension2 = video.file_name;
-                        var regex2 = /\.[A-Za-z0-9]+$/gm
-                        var vidtext2 = exstension2.replace(regex2, '');
-                        
-                        var file_name2 = `${vidtext2}`;
-                        if(ctx.message.caption == undefined){
-                            var caption2 =  ``;
-                        }else{
-                            var caption2 =  `\n\n${ctx.message.caption}`;
-                        }
-                    }
-
                     await ctx.reply(`Thank you for sending.\nSearch #file${result}`,{
                         chat_id: ctx.chat.id,
                         parse_mode: 'HTML',
