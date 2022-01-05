28dfe8d0ae8620447dcd070f1ec23a1af53f7836
BimoSora <bimosora3@gmail.com>
2022-01-05 12:04:32 +0700

    first commit

--- index.js
+++ index.js
@@ -1283,13 +1283,13 @@ bot.on('video', async(ctx) => {
     if(ctx.chat.type == 'private') {
         if(ctx.from.id == Number(process.env.ADMIN) || ctx.from.id == Number(process.env.ADMIN1) || ctx.from.id == Number(process.env.ADMIN2) || ctx.from.id == Number(process.env.ADMIN3) || ctx.from.id == Number(process.env.ADMIN4)){
             const { file_name, file_id, file_unique_id, file_size } = ctx.message.video
-            const { caption } = ctx.message.caption // <-- store data in temporary varibles 
+            var { caption } = ctx.message.caption // <-- store data in temporary varibles 
             if(file_name == undefined){
                 const file_name2 = `${today2(ctx)}`;
                 if(caption == undefined){
-                    let caption2 =  ``;
+                    var caption2 =  ``;
                 }else{
-                    let caption2 =  `\n\n${caption}`;
+                    var caption2 =  `\n\n${caption}`;
                 }
             }else{
                 const exstension2 = file_name;
@@ -1298,9 +1298,9 @@ bot.on('video', async(ctx) => {
                 
                 const file_name2 = `${vidtext2}`;
                 if(caption == undefined){
-                    let caption2 =  ``;
+                    var caption2 =  ``;
                 }else{
-                    let caption2 =  `\n\n${caption}`;
+                    var caption2 =  `\n\n${caption}`;
                 }
             }
               
