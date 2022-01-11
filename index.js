11017f86452043965127aa9323347a82c02c54f5
BimoSora <bimosora3@gmail.com>
2022-01-11 07:17:00 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1132,7 +1132,7 @@ bot.command('url', async ctx => {
     } catch (e) {
       console.error(e);
     }
-    if (mime.startsWith('document' || 'video' || 'photo')) {
+    if (mime.startsWith('video')) {
       await ctx.replyWithDocument({
         source: buffer,
         filename: `${filename2}`
